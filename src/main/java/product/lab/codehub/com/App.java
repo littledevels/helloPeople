a5d6361f544e172d93bfcd4c9ec79e03e555f9c0
Kostas Makedos <kostas.makedos@yahoo.com>
2019-04-04 21:18:28 +0000

    Initial commit

---
+++ src/main/java/product/lab/codehub/com/App.java
@@ -0,0 +1,13 @@
+package product.lab.codehub.com;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello People!" );
+    }
+}
